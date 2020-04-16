376f37991d985192b4f233b7892bc097af6679b3
Mark Lopez <m@silvenga.com>
2020-04-16 10:07:58 -0500

    Added repo-case.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting...");
+                Thread.Sleep(1000);
+            }
+
+            // ReSharper disable once FunctionNeverReturns
+        }
+    }
+}
